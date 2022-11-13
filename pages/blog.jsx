94ef6aa1b8536123e9369ca92041e38c118d0101
matthew <mematthew123@icloud.com>
2022-11-12 19:57:53 -0700

    added sanity cms for blogs

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,15 +1,78 @@
-import React from 'react'
+import imageUrlBuilder from '@sanity/image-url'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-const blog = () => {
+export default function Blog({ posts }) {
+  const router = useRouter()
+  const [mappedPosts, setMappedPosts] = useState([])
+
+  useEffect(() => {
+    if (posts.length) {
+      const imgBuilder = imageUrlBuilder({
+        projectId: 'qx1vzyzi',
+        dataset: 'production',
+      })
+
+      setMappedPosts(
+        posts.map((p) => {
+          return {
+            ...p,
+            mainImage: imgBuilder.image(p.mainImage).width(800).height(650),
+          }
+        })
+      )
+    } else {
+      setMappedPosts([])
+    }
+  }, [posts])
+
+  // return (
+
   return (
-    // WE ARE GOING TO MAKE THIS A COMING SOON PAGE FOE NOW
-    <div className='flex justify-center items-center h-screen'>
-        <h1 className='text-4xl font-bold'>COMING SOON</h1>
-        
-    
-    
+    <div className="flex flex-col md:flex-row col-auto justify-between ">
+      <div className="min-h-screen bottom-6 mx-auto max-w-7xl px-4 sm:mt-24 md:mt-24 text-center justify-center ">
+        <h1 className="font-extrabold text-gray-900">
+          <p className="text-xl sm:text-3xl md:text-4xl">Blogs && Dogs</p>
+        </h1>
+        <h3 className="text-xl sm:text-3xl md:text-4xl pt-1">Recent Posts:</h3>
+
+        <div className=" justify-center grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 pt-8">
+          {mappedPosts.length ? (
+            mappedPosts.map((p, index) => (
+              <div
+                onClick={() => router.push(`/post/${p.slug.current}`)}
+                key={index}
+                className=" inline-grid justify-center rounded-lg shadow-lg bg-white p-8 hover:cursor-pointer "
+              >
+                <h3 className=" inline-grid font-bold text-xl text-slate-800 ">{p.title}</h3>
+                <img src={p.mainImage} alt={p.title} />
+              </div>
+            ))
+          ) : (
+            <>No Posts Yet</>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
 
-export default blog
+export const getServerSideProps = async (pageContext) => {
+  const query = encodeURIComponent('*[ _type == "post" ]')
+  const url = `https://qx1vzyzi.api.sanity.io/v2021-06-07/data/query/production?query=${query}`
+  const result = await fetch(url).then((res) => res.json())
+
+  if (!result.result || !result.result.length) {
+    return {
+      props: {
+        posts: [],
+      },
+    }
+  } else {
+    return {
+      props: {
+        posts: result.result,
+      },
+    }
+  }
+}
